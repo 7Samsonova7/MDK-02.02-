91a15486d1771ab4407afc234b84058c08fa7454
7Samsonova7 <asamsonova82@gmail.com>
2026-09-02 21:35:29 +0300

    Создать проект

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double rubles = 30002.7;
+        double kursDollar = 78.5;
+        double dollars = rubles/ kursDollar;
+        System.out.println("У вас на счету " + rubles + "рублей. В долларах это " + dollars + " долларов.Так держать!");
+    }
+}
---
+++ src/Zadanie3.java
@@ -0,0 +1,12 @@
+public class Zadanie3 {
+    public static void main(String[] args) {
+        int eggs = 4;
+        if (eggs > 3) {
+            System.out.println("Рекомендую приготовить омлет.");}
+        else {
+            System.out.println("Рекомендую позавтракать будетбродами.");
+            }
+        System.out.println("Приятнго аппетита!");
+        }
+    }
+
